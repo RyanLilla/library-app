b79141c25136eaeb4cc7ab5c04a6821b8d20ea93
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-01-21 13:32:56 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
+
+window = tk.Tk()
+window.title("Library-App")
+
+# title - label
+
+
+# title - entry
+
+# author - label
+
+# author - entry
+
+# buttoms
+
+# table to display the list of books
+
+
+window.mainloop()

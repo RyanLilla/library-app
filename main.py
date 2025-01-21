aa7960448334c3c60ad771c50067eeebb7b05867
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-01-21 14:54:24 -0700

    Add .gitignore and update main.py layout for title and author entries

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from tkinter import messagebox, ttk
 window = tk.Tk()
 window.title("Library-App")
 
-# title - label
+# title - label and entry
+title_label = tk.Label(window, text="Title")
+title_label.grid(row=0, column=0)
+title_entry = tk.Entry(window)
+title_entry.grid(row=0, column=1)
 
+# author - label and entry
+author_label = tk.Label(window, text="Author")
+author_label.grid(row=1, column=0)
+author_entry = tk.Entry(window)
+author_entry.grid(row=1, column=1)
 
-# title - entry
-
-# author - label
-
-# author - entry
-
-# buttoms
+# buttons
 
 # table to display the list of books
 

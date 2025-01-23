64053eff1eb46c3be184fd3f5ab97287bd68cd37
Ryan Lilla <7048518+RyanLilla@users.noreply.github.com>
2025-01-23 15:19:27 -0700

    Implement add_book and remove_book methods; add file I/O for saving and loading books

--- Library.py
+++ Library.py
@@ -1,18 +1,31 @@
+from book import Book
+
+
 class Library:
     def __init__(self):
         self.books = []
     
-    def add_book(self):
-        pass
+    def add_book(self, book):
+        self.books.append(book)
+        self.save_books_to_file()
     
-    def remove_book(self):
-        pass
+    def remove_book(self, book):
+        self.books = [b for b in self.books if b.title != book.title or b.author != book.author]
     
     def list_books(self):
-        pass
+        return self.books
     
     def save_books_to_file(self):
-        pass
+        with open("library.txt", "w") as file:
+            for book in self.books:
+                file.write(f"{book.title},{book.author}\n")
     
     def load_books_from_file(self):
-        pass
+        try:
+            with open("library.txt", "r") as file:
+                for line in file:
+                    title, author = line.strip().split(",")
+                    book = Book(title, author)
+                    self.books.append(book)
+        except FileNotFoundError:
+            pass
